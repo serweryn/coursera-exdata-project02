aaa894de000171ff07e7785c45bd6e65458d08ce
Seweryn Sobieszek <serweryn@gmail.com>
2015-08-23 11:24:58 +0200

    add comments

--- plot1.R
+++ plot1.R
@@ -1,17 +1,19 @@
+# load datasets only if they aren't 
 if (!exists("NEI")) {
   NEI <- readRDS("summarySCC_PM25.rds")
 }
-
 if (!exists("SCC")) {
   SCC <- readRDS("Source_Classification_Code.rds")
 }
 
-library(plyr)
+library(plyr) # required for ddply
 
+# aggreate emmissions over years
 aggEmissions <- ddply(NEI, ~year, summarise, Emissions=sum(Emissions))
 
 png(file = "plot1.png", width = 480, height = 480)
 
+# plot emissions over years
 plot(aggEmissions$year, aggEmissions$Emissions, type="h", lwd=5, col="red",
      main="PM2.5 Emissions in US", xlab="Year", ylab="Emissions in year")
 
