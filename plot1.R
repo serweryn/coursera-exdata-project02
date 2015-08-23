994f634de5f08a2f4298fe003d2f5348c8f4d66d
Seweryn Sobieszek <serweryn@gmail.com>
2015-08-23 11:14:21 +0200

    Better y label

--- plot1.R
+++ plot1.R
@@ -13,6 +13,6 @@ aggEmissions <- ddply(NEI, ~year, summarise, Emissions=sum(Emissions))
 png(file = "plot1.png", width = 480, height = 480)
 
 plot(aggEmissions$year, aggEmissions$Emissions, type="h", lwd=5, col="red",
-     main="PM2.5 Emissions in US", xlab="Year", ylab="Emissions")
+     main="PM2.5 Emissions in US", xlab="Year", ylab="Emissions in year")
 
 dev.off()
